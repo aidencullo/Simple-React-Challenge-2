d590189b6d23ca1bdec39982b0424edd838ab2b8
Aiden Cullo <aidencullo@gmail.com>
2024-09-17 13:57:40 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+export default function PerScholasLogo() {
+  return;
+  <img
+    src="https://perscholas.org/wp-content/themes/per-scholas/assets/images/logo1.svg"
+    alt="Per Scholas"
+  />;
+}
