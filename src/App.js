a7dd027b72214eca7f7998982b48a316eeec3cb9
Aiden Cullo <aidencullo@gmail.com>
2024-09-17 14:07:55 -0400

    fix typo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 export default function PerScholasLogo() {
-  return;
-  <img
+  return (
+    <img
     src="https://perscholas.org/wp-content/themes/per-scholas/assets/images/logo1.svg"
     alt="Per Scholas"
-  />;
+      />
+  );
 }
